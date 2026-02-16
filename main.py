b1a2377e950d319940e50c13b70ec51392b22529
jumusk691-lgtm <jumusk691@gmail.com>
2026-02-16 19:54:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,3 +71,6 @@ if __name__ == '__main__':
             print("❌ Angel Login Failed")
     except Exception as e:
         print(f"❌ Critical Error: {e}")
+
+if __name__ == "__main__":
+    app.run()
