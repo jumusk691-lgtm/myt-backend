ffb6058121a057c72446d1b4f80c0becd8ed5095
Skjumu <jumusk691@gmail.com>
2026-02-17 19:41:16 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import eventlet
-eventlet.monkey_patch()  # यह ट्रेडिंग WebSocket के लिए बहुत ज़रूरी है
-
-from main import app  # यह आपकी main.py से app को लोड करेगा
-
-if __name__ == "__main__":
-    app.run()
